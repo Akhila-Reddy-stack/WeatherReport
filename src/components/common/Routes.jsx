1e08b869c6dc6db696d389682261d4150d2c42f1
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-05-01 16:45:35 +0530

    Add files via upload

--- src/components/common/Routes.jsx
+++ src/components/common/Routes.jsx
@@ -1,1 +1,26 @@
+import React, { PureComponent } from "react";
+import { Route, Switch } from "react-router-dom";
+import WeatherReport from "../weather/WeatherReport";
 
+
+class Routes extends PureComponent {
+  constructor(props) {
+    super(props);
+  this.state = {
+
+  }
+}
+
+  render() {
+    console.log(this.props)
+    return (
+      <Switch>
+       <Route path="/" exact component={WeatherReport} /> 
+       <Route path="/weatherReport" exact component={WeatherReport} /> 
+       
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
