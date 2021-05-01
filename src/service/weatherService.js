29fcbea4ece3a309e017c64f25f212275e4a20d4
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-05-01 16:47:49 +0530

    Add files via upload

--- src/service/weatherService.js
+++ src/service/weatherService.js
@@ -1,1 +1,19 @@
+import http from './httpService';
+import { apiUrl } from "../config.json";
+
+export const getWeatherReport = () => {    
+  const apiEndPoint = `${apiUrl}/weather?q=London&appid=44ee963ea8384bd561e7cce2a9b68f08`;
+  return http.get(apiEndPoint);
+}
+
+export const searchString = (searchString) => {
+  const apiEndPoint = `${apiUrl}/weather?searchString=${searchString}`;
+  console.log(";;;;;;;;;;;; data", apiEndPoint)
+  return http.get(`${apiEndPoint}`)
+}
+
+
+
+
+
 
